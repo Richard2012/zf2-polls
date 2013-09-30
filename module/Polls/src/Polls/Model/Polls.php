1ed54573447d298c5510089c8c5e9505dcbc0970
Caribe Digital <iteam@gmx.com>
2013-09-29 22:19:41 -0400

    database polls with 3 tables and Polls Table

---
+++ module/Polls/src/Polls/Model/Polls.php
@@ -0,0 +1,92 @@
+<?php
+namespace Polls\Model;
+
+use Zend\InputFilter\Factory as InputFactory;
+use Zend\InputFilter\InputFilter;
+use Zend\InputFilter\InputFilterInterface;
+use Zend\InputFilter\InputFilterAwareInterface;
+
+class Polls implements InputFilterAwareInterface
+{
+    public $id;
+    public $name;
+    public $question;
+    protected $inputFilter;
+
+    public function exchangeArray($data)
+    {
+        $this->id          = (isset($data['id'])) ? $data['id'] : null;
+        $this->name        = (isset($data['name'])) ? $data['name'] : null;
+        $this->question    = (isset($data['question'])) ? $data['question'] : null;
+        $this->server_name = (isset($data['server_name'])) ? $data['server_name'] : null;
+    }
+
+    public function setInputFilter(InputFilterInterface $inputFilter)
+    {
+        throw new \Exception("Not used");
+    }
+
+    public function getArrayCopy()
+    {
+        return get_object_vars($this);
+    }
+
+
+    public function getInputFilter()
+    {
+        if (!$this->inputFilter) {
+            $inputFilter = new InputFilter();
+            $factory     = new InputFactory();
+
+            $inputFilter->add($factory->createInput(array(
+                'name'     => 'id',
+                'required' => true,
+                'filters'  => array(
+                    array('name' => 'Int'),
+                ),
+            )));
+
+            $inputFilter->add($factory->createInput(array(
+                'name'     => 'name',
+                'required' => true,
+                'filters'  => array(
+                    array('name' => 'StripTags'),
+                    array('name' => 'StringTrim'),
+                ),
+                'validators' => array(
+                    array(
+                        'name'    => 'StringLength',
+                        'options' => array(
+                            'encoding' => 'UTF-8',
+                            'min'      => 1,
+                            'max'      => 255,
+                        ),
+                    ),
+                ),
+            )));
+
+            $inputFilter->add($factory->createInput(array(
+                'name'     => 'question',
+                'required' => true,
+                'filters'  => array(
+                    array('name' => 'StripTags'),
+                    array('name' => 'StringTrim'),
+                ),
+                'validators' => array(
+                    array(
+                        'name'    => 'StringLength',
+                        'options' => array(
+                            'encoding' => 'UTF-8',
+                            'min'      => 1,
+                            'max'      => 255,
+                        ),
+                    ),
+                ),
+            )));
+
+            $this->inputFilter = $inputFilter;
+        }
+
+        return $this->inputFilter;
+    }
+}
